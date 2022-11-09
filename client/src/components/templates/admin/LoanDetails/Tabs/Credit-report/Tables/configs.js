715c4198eca127836b935dbd9eab9a20d909913c
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-11-09 11:01:51 -0400

    updated table config

--- client/src/components/templates/admin/LoanDetails/Tabs/Credit-report/Tables/configs.js
+++ client/src/components/templates/admin/LoanDetails/Tabs/Credit-report/Tables/configs.js
@@ -1,4 +1,10 @@
-export const userInfo = ({ firstName = "--", LastName = "--", MiddleName = "--", creditScore = "--", ssnNumber = "--" }) => [
+export const userInfo = ({
+  firstName = "--",
+  LastName = "--",
+  MiddleName = "--",
+  creditScore = "--",
+  ssnNumber = "--",
+}) => [
   { label: "First Name", value: firstName },
   { label: "Last Name", value: LastName },
   { label: "Middle Name", value: MiddleName },
@@ -6,8 +12,24 @@ export const userInfo = ({ firstName = "--", LastName = "--", MiddleName = "--",
   { label: "Credit score	", value: creditScore },
 ];
 
-export const tradeInfo = ({ ECOADesignator = "--", subscriber = {}, accountNumber = "--", accountRating = "--", currentBalance = "--", highCredit = "--", pastDue = "--", dateOpened, dateClosed, datePayedOut, portfolioType = "--", creditLimit = "--" }) => [
-  { label: "Subscriber Industry Code", value: subscriber?.industryCode || "--" },
+export const tradeInfo = ({
+  ECOADesignator = "--",
+  subscriber = {},
+  accountNumber = "--",
+  accountRating = "--",
+  currentBalance = "--",
+  highCredit = "--",
+  pastDue = "--",
+  dateOpened,
+  dateClosed,
+  datePayedOut,
+  portfolioType = "--",
+  creditLimit = "--",
+}) => [
+  {
+    label: "Subscriber Industry Code",
+    value: subscriber?.industryCode || "--",
+  },
   { label: "Subscriber Member Code", value: subscriber?.memberCode || "--" },
   { label: "Subscriber Name", value: subscriber?.name?.unparsed || "--" },
   { label: "Portfolio Type", value: portfolioType },
@@ -23,7 +45,13 @@ export const tradeInfo = ({ ECOADesignator = "--", subscriber = {}, accountNumbe
   { label: "Past Due", value: pastDue },
 ];
 
-export const employmentInfo = ({ employer, occupation = "--", dateHired = "--", dateOnFileSince, dateEffective }) => [
+export const employmentInfo = ({
+  employer,
+  occupation = "--",
+  dateHired = "--",
+  dateOnFileSince,
+  dateEffective,
+}) => [
   { label: "Employer", value: employer?.unparsed || "--" },
   { label: "Occupation", value: occupation },
   { label: "Date Hired", value: dateHired },
