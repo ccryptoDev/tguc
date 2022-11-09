77614fae7b9afef95e8edbf63a1e30526ccb860e
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-11-09 11:01:37 -0400

    modified offer template

--- client/src/components/templates/admin/Forms/ManageRules/Offers/options.js
+++ client/src/components/templates/admin/Forms/ManageRules/Offers/options.js
@@ -1,36 +1,36 @@
 export const optionsAPlus = [
-  { value: "1", id: "1", text: "APR - 9.99 Dealer Discount - 0.00%" },
-  { value: "2", id: "2", text: "APR - 8.99 Dealer Discount - 3.00%" },
-  { value: "3", id: "3", text: "APR - 7.99 Dealer Discount - 6.00%" },
-  { value: "4", id: "4", text: "APR - 6.99 Dealer Discount - 9.00%" },
-  { value: "5", id: "5", text: "APR - 5.99 Dealer Discount - 12.00%" },
+  { value: "8.99", id: "2", text: "APR - 8.99 Dealer Discount - 3.00%" },
+  { value: "9.99", id: "1", text: "APR - 9.99 Dealer Discount - 0.00%" },
+  { value: "7.99", id: "3", text: "APR - 7.99 Dealer Discount - 6.00%" },
+  { value: "6.99", id: "4", text: "APR - 6.99 Dealer Discount - 9.00%" },
+  { value: "5.99", id: "5", text: "APR - 5.99 Dealer Discount - 12.00%" },
 ];
 export const optionsA = [
-  { value: "1", id: "1", text: "APR - 10.99 Dealer Discount - 0.00%" },
-  { value: "2", id: "2", text: "APR - 9.99 Dealer Discount - 3.00%" },
-  { value: "3", id: "3", text: "APR - 8.99 Dealer Discount - 6.00%" },
-  { value: "4", id: "4", text: "APR - 7.99 Dealer Discount - 9.00%" },
-  { value: "5", id: "5", text: "APR - 6.99 Dealer Discount - 12.00%" },
+  { value: "10.99", id: "1", text: "APR - 10.99 Dealer Discount - 0.00%" },
+  { value: "9.99", id: "2", text: "APR - 9.99 Dealer Discount - 3.00%" },
+  { value: "8.99", id: "3", text: "APR - 8.99 Dealer Discount - 6.00%" },
+  { value: "7.99", id: "4", text: "APR - 7.99 Dealer Discount - 9.00%" },
+  { value: "6.99", id: "5", text: "APR - 6.99 Dealer Discount - 12.00%" },
 ];
 export const optionsB = [
-  { value: "1", id: "1", text: "APR - 12.99 Dealer Discount - 0.00%" },
-  { value: "2", id: "2", text: "APR - 11.99 Dealer Discount - 3.00%" },
-  { value: "3", id: "3", text: "APR - 10.99 Dealer Discount - 6.00%" },
-  { value: "4", id: "4", text: "APR - 9.99 Dealer Discount - 9.00%" },
-  { value: "5", id: "5", text: "APR - 8.99 Dealer Discount - 12.00%" },
+  { value: "12.99", id: "1", text: "APR - 12.99 Dealer Discount - 0.00%" },
+  { value: "11.99", id: "2", text: "APR - 11.99 Dealer Discount - 3.00%" },
+  { value: "10.99", id: "3", text: "APR - 10.99 Dealer Discount - 6.00%" },
+  { value: "9.99", id: "4", text: "APR - 9.99 Dealer Discount - 9.00%" },
+  { value: "8.99", id: "5", text: "APR - 8.99 Dealer Discount - 12.00%" },
 ];
 export const optionsC = [
-  { value: "1", id: "1", text: "APR - 14.99 Dealer Discount - 0.00%" },
-  { value: "2", id: "2", text: "APR - 13.99 Dealer Discount - 3.00%" },
-  { value: "3", id: "3", text: "APR - 12.99 Dealer Discount - 6.00%" },
-  { value: "4", id: "4", text: "APR - 11.99 Dealer Discount - 9.00%" },
-  { value: "5", id: "5", text: "APR - 10.99 Dealer Discount - 12.00%" },
+  { value: "14.99", id: "1", text: "APR - 14.99 Dealer Discount - 0.00%" },
+  { value: "13.99", id: "2", text: "APR - 13.99 Dealer Discount - 3.00%" },
+  { value: "12.99", id: "3", text: "APR - 12.99 Dealer Discount - 6.00%" },
+  { value: "11.99", id: "4", text: "APR - 11.99 Dealer Discount - 9.00%" },
+  { value: "10.99", id: "5", text: "APR - 10.99 Dealer Discount - 12.00%" },
 ];
 export const optionsD = [
-  { value: "1", id: "1", text: "APR - 16.99 Dealer Discount - 9.00%" },
-  { value: "2", id: "2", text: "Block Offers to D Tier" },
+  { value: "16.99", id: "1", text: "APR - 16.99 Dealer Discount - 9.00%" },
+  { value: "-1", id: "2", text: "Block Offers to D Tier" },
 ];
 export const optionsE = [
-  { value: "1", id: "1", text: "APR - 19.99 Dealer Discount - 12.00%" },
-  { value: "2", id: "2", text: "Block Offers to E Tier" },
+  { value: "19.99", id: "1", text: "APR - 19.99 Dealer Discount - 12.00%" },
+  { value: "-1", id: "2", text: "Block Offers to E Tier" },
 ];
