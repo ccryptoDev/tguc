a63d26b97f6aa86470614cc3be11dd0a5162ea7f
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-11-15 06:05:07 -0400

    updated loanpaymentpro controller

--- server/src/loans/payments/loanpaymentpro/loanpaymentpro.controller.ts
+++ server/src/loans/payments/loanpaymentpro/loanpaymentpro.controller.ts
@@ -23,6 +23,7 @@ import { LoanpaymentproService } from './loanpaymentpro.service';
 import { LoanPaymentProCardToken } from './loanpaymentpro-card-token.entity';
 import { AddCardDto } from './validation/addCard.dto';
 import { ScreenTracking } from '../../../user/screen-tracking/entities/screen-tracking.entity';
+import { Card } from '../../../user/cards/entities/card.entity';
 import { BadRequestResponse } from '../../../types/bad-request-response';
 import { ErrorResponse } from '../../../types/error-response';
 
@@ -33,6 +34,8 @@ export class LoanpaymentproController {
   constructor(
     @InjectRepository(ScreenTracking)
     private readonly screenTrackingModel: Repository<ScreenTracking>,
+    @InjectRepository(Card)
+    private readonly cardModel : Repository<Card>,
     private readonly loanPaymentProService: LoanpaymentproService,
     private readonly logger: LoggerService,
   ) {}
@@ -42,20 +45,47 @@ export class LoanpaymentproController {
   @ApiInternalServerErrorResponse({ type: ErrorResponse })
   @Post('addCard')
   @UsePipes(new ValidationPipe())
-  async addCard(@Body() addCardDto: AddCardDto, @Req() request: Request) {
+  async addCard(@Body() data: any, @Req() request: Request) {
     const user = request.user.id;
-    addCardDto.screenTrackingId = request.user.screenTracking;
+    data.screenTrackingId = request.user.screenTracking;
     try {
-      const response: LoanPaymentProCardToken =
+      /*const response: LoanPaymentProCardToken =
         await this.loanPaymentProService.v2PaymentCardsAdd(
           addCardDto,
           request.id,
-        );
-
+        );*/
+        let response;
+        let obj = {
+          screenTracking: data?.screenTrackingId,
+          cardNumber : data?.cardNumber?.value,
+          cardName : data?.fullName?.value,
+          accountNumber : data?.accountNumber?.value,
+          routingNumber : data?.routingNumber?.value,
+          financialInstitution : data?.financialInstitution?.value,
+          accountType : data?.accountType?.value,
+          manualPayment : data?.manualPayment?.value,
+          expiryDate : data?.expirationDate?.value,
+          securityCode : data?.securityCode?.value,
+          billingAddress1 : data?.street?.value,
+          billingCity : data?.city?.value,
+          billingFirstName: data?.firstName?.value,
+          billingLastName : data?.lastName?.value,
+          billingState : data?.state?.value,
+          billingZip : data?.zipCode?.value
+        }
+      let exist = await this.cardModel.findOne({screenTracking: data.screenTrackingId});
+      if (exist) {
+        response = await this.cardModel.update(exist.id,
+          obj
+        )
+      } else {
+        let create = await this.cardModel.create(obj);
+        response = await this.cardModel.save(create);
+      }
       await this.screenTrackingModel.update(
         { user },
         {
-          lastLevel: 'document-upload',
+          lastScreen: 'sign-contract',
         },
       );
 
